e622bc406501d6a6f9f744942b41f0beb40cc956
Someshwari <someshwariwarwatte@gmail.com>
2025-10-05 12:48:08 +0530

    Create CaseChange.java

---
+++ CaseChange.java
@@ -0,0 +1,7 @@
+public class CaseChange {
+    public static void main(String[] args) {
+        String str = "Hello Java";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+    }
+}
