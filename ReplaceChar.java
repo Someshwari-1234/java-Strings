08b46545633baf50c9898eb93f888845ab353c05
Someshwari <someshwariwarwatte@gmail.com>
2025-10-06 22:59:19 +0530

    Create ReplaceChar.java

---
+++ ReplaceChar.java
@@ -0,0 +1,7 @@
+public class ReplaceChar {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        String newStr = str.replace('a', 'x');
+        System.out.println("After replace: " + newStr);
+    }
+}
