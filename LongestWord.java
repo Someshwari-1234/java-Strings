681097db3032eed519066eb716b3774990c808cd
Someshwari <someshwariwarwatte@gmail.com>
2025-10-11 23:08:27 +0530

    Create LongestWord.java

---
+++ LongestWord.java
@@ -0,0 +1,13 @@
+public class LongestWord {
+    public static void main(String[] args) {
+        String sentence = "Java is powerful";
+        String[] words = sentence.split(" ");
+        String longest = "";
+
+        for (String w : words)
+            if (w.length() > longest.length())
+                longest = w;
+
+        System.out.println("Longest word: " + longest);
+    }
+}
