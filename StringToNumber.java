ec704c4c9f1c8c40ded19c7ebf9459a1be33b8a9
Someshwari <someshwariwarwatte@gmail.com>
2025-10-16 23:00:24 +0530

    Create StringToNumber.java

---
+++ StringToNumber.java
@@ -0,0 +1,8 @@
+public class StringToNumber {
+    public static void main(String[] args) {
+        String str1 = "25";
+        String str2 = "15";
+        int sum = Integer.parseInt(str1) + Integer.parseInt(str2);
+        System.out.println("Sum = " + sum);
+    }
+}
