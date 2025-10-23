24f4ccb811b84cd9ce5af2b1c9039bd22c4a40f2
Someshwari <someshwariwarwatte@gmail.com>
2025-10-23 21:49:10 +0530

    Add RightTriangleString class to print triangle pattern

---
+++ RightTriangleString.java
@@ -0,0 +1,11 @@
+public class RightTriangleString {
+    public static void main(String[] args) {
+        String s = "HELLO";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(s.charAt(j));
+            }
+            System.out.println();
+        }
+    }
+}
