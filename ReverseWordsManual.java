958ed460c6091d1d4070ddb6a0d3679b5dc4a741
Someshwari <someshwariwarwatte@gmail.com>
2025-10-25 15:31:16 +0530

    Create ReverseWordsManual.java

---
+++ ReverseWordsManual.java
@@ -0,0 +1,25 @@
+public class ReverseWordsManual {
+    public static void main(String[] args) {
+        String s = "Java is fun";
+        s = s + " ";
+        String word = "";
+        String result = "";
+        String[] words = new String[10];
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c != ' ')
+                word = word + c;
+            else {
+                words[count++] = word;
+                word = "";
+            }
+        }
+
+        for (int i = count - 1; i >= 0; i--)
+            result = result + words[i] + " ";
+
+        System.out.println(result.trim());
+    }
+}
